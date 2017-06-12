a019d133994fdcba303d44e5056f0fe8c834ef79
jcmdln <jcmdln@gmail.com>
2017-06-12 08:55:49 -0400

    update symlinks

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-cdur=`pwd`
-
-#ln -sf $cdur/bash/bashrc
-ln -sf $cdur/emacs/init.el  ~/.emacs.d/init.el
-ln -sf $cdur/tmux/tmux.conf ~/.tmux.conf
-ln -sf $cdur/vim/vimrc      ~/.vimrc
+ln -sf $(pwd)/bash/bashrc    ~/.bashrc
+ln -sf $(pwd)/emacs/init.el  ~/.emacs.d/init.el
+ln -sf $(pwd)/tmux/tmux.conf ~/.tmux.conf
+ln -sf $(pwd)/vim/vimrc      ~/.vimrc
+ln -sf $(pwd)/zsh/zshrc      ~/.zshrc
